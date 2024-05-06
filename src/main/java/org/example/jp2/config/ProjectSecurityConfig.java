409169f6233065168297b71f886420ed264316b7
KJ_Swaroop <jeevanaswaroop45@gmail.com>
2024-05-06 11:01:42 -0700

    section 2

--- src/main/java/org/example/jp2/config/ProjectSecurityConfig.java
+++ src/main/java/org/example/jp2/config/ProjectSecurityConfig.java
@@ -1,10 +1,48 @@
 package org.example.jp2.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectSecurityConfig {
 
+    @Bean
+    public SecurityFilterChain custonSecurityFilterChain(HttpSecurity http) throws Exception {
 
+        http.authorizeHttpRequests((request)->{
+            request.requestMatchers("/myAccount").permitAll()
+                    .anyRequest().authenticated();
+
+        }).formLogin(Customizer.withDefaults())
+                .logout(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+
+        UserDetails user1 = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("1234")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, admin);
+
+    }
 
 }
