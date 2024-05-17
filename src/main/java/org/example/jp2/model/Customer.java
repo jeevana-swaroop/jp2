9968165eeeb3fa1fc32e833283c500bff009f042
KJ_Swaroop <jeevanaswaroop45@gmail.com>
2024-05-16 19:33:35 -0700

    latest

--- src/main/java/org/example/jp2/model/Customer.java
+++ src/main/java/org/example/jp2/model/Customer.java
@@ -1,10 +1,8 @@
 package org.example.jp2.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.GenericGenerator;
 
 @Table
@@ -17,8 +15,13 @@ public class Customer {
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
     @GenericGenerator(name = "native", strategy = "native")
     private int id;
+
+    @Column(unique = true, nullable = false)
     private String email;
+
+    @Column(unique = true, nullable = false)
     private String pwd;
+
     private String role;
 
 
